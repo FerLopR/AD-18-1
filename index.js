79105af99661c5139522ca7bfa4ab7ce339cea04
Fernando López <fernandboy18@gmail.com>
2025-09-28 23:06:16 -0600

    Entrega lab

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const itemsContainer = document.querySelector("#list-items")
+const itemsContainer = document.querySelector("#list-items") //selección del contenedor para almacenar en ese nodo.
 
+//Crea un item para recibir cada color del API
 function addItem(item) {
   const colourCard = document.createElement("section")
   colourCard.className = "card w-75"
@@ -28,13 +29,65 @@ function addItem(item) {
   itemsContainer.append(colourCardBreak)
 }
 
-function fetchColorsList() {
-  
+
+async function fetchColorsList() {
+  const url = "https://reqres.in/api/unknown";
+  //petición 
+  try{
+    const response = await fetch(url, {
+      headers:{
+        "x-api-key": "reqres-free-v1"
+      }
+    });
+    if(!response.ok){
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const result = await response.json(); //convierte la respuesta en un objeto JS
+    console.log(result);
+
+    //El array que contiene todo los colores
+    //.forEach(color => {}) recorre cada valor
+    result.data.forEach(color => {
+      addItem(color); //crea la tarjeta para cada color
+
+
+    //Se configura para almacenar los colores al localStorage   
+    localStorage.setItem("colors", JSON.stringify(result.data));
+      
+    });
+     
+  }catch(error){
+    console.log(error.message);
+  }
 }
 
+
+//función para cargar desde localStorage
 function loadColorsFromStorage() {
+  const addColors = localStorage.getItem("colors"); //recupera lo almacenado 
+
+//Validación
+if(addColors){
+  const colorsArray = JSON.parse(addColors);
+
+  colorsArray.forEach( color => {
+    addItem(color);
+  });
+}else{
+  console.log("No se guardadron los colores")
+}
   
 }
 
+
+borrarColores = () =>{
+  itemsContainer.innerHTML="";
+  localStorage.removeItem("colors");
+  console.log("Colores eliminados");
+}
+const miBtn = document.getElementById("Button");
+miBtn.addEventListener("click", borrarColores);
+//Echa a andar este código (corre la funciones)
 fetchColorsList()
 loadColorsFromStorage()
